3319daa1826ead7da2b6c05fc5720888ae9c659f
Jonilson Sousa <jonilsonds9@gmail.com>
2021-06-01 16:12:09 -0300

    Entendendo Lazy e Eager

---
+++ src/main/java/br/com/alura/loja/testes/PerformanceConsultas.java
@@ -0,0 +1,60 @@
+package br.com.alura.loja.testes;
+
+import br.com.alura.loja.dao.*;
+import br.com.alura.loja.modelo.*;
+import br.com.alura.loja.util.JPAUtil;
+
+import javax.persistence.EntityManager;
+import java.math.BigDecimal;
+
+public class PerformanceConsultas {
+
+    public static void main(String[] args) {
+        popularBancoDeDados();
+        EntityManager em = JPAUtil.getEntityManager();
+        Pedido pedido = em.find(Pedido.class, 1l);
+        System.out.println(pedido.getItens().size());
+    }
+
+    private static void popularBancoDeDados() {
+        Categoria celulares = new Categoria("CELULARES");
+        Categoria videogames = new Categoria("VIDEOGAMES");
+        Categoria informatica = new Categoria("INFORMATICA");
+
+        Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), celulares);
+        Produto videogame = new Produto("PS5", "Playstation 5", new BigDecimal("8000"), videogames);
+        Produto macbook = new Produto("Macbook", "Macboo pro retina", new BigDecimal("14000"), informatica);
+
+        Cliente cliente = new Cliente("Jonilson", "123456");
+
+        Pedido pedido = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(10, pedido, celular));
+        pedido.adicionarItem(new ItemPedido(40, pedido, videogame));
+
+        Pedido pedido2 = new Pedido(cliente);
+        pedido.adicionarItem(new ItemPedido(2, pedido, macbook));
+
+        EntityManager em = JPAUtil.getEntityManager();
+        ProdutoDao produtoDao = new ProdutoDao(em);
+        CategoriaDao categoriaDao = new CategoriaDao(em);
+        ClienteDao clienteDao = new ClienteDao(em);
+        PedidoDao pedidoDao = new PedidoDao(em);
+
+        em.getTransaction().begin();
+
+        categoriaDao.cadastrar(celulares);
+        categoriaDao.cadastrar(videogames);
+        categoriaDao.cadastrar(informatica);
+
+        produtoDao.cadastrar(celular);
+        produtoDao.cadastrar(videogame);
+        produtoDao.cadastrar(macbook);
+
+        clienteDao.cadastrar(cliente);
+
+        pedidoDao.cadastrar(pedido);
+
+        em.getTransaction().commit();
+        em.close();
+    }
+}
