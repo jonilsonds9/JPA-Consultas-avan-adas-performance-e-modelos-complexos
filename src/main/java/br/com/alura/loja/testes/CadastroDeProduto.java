308cd6b50c32afabd7360cdadaf04fbdd64027f3
Jonilson Sousa <jonilsonds9@gmail.com>
2021-06-02 09:51:10 -0300

    Mapeamento de chaves compostas

--- src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -7,8 +7,7 @@ import javax.persistence.EntityManager;
 
 import br.com.alura.loja.dao.CategoriaDao;
 import br.com.alura.loja.dao.ProdutoDao;
-import br.com.alura.loja.modelo.Categoria;
-import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.modelo.*;
 import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
@@ -42,6 +41,9 @@ public class CadastroDeProduto {
 		produtoDao.cadastrar(celular);
 		
 		em.getTransaction().commit();
+
+		em.find(Categoria.class, new CategoriaId("CELULARES", "xpto"));
+
 		em.close();
 	}
 
